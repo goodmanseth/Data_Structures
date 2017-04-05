cd138d18375f56cf314791f0bdc9896b3e329568
Seth Goodman <sg277@duke.edu>
2017-04-05 15:35:01 -0400

    added private Node class

--- src/ds/queue/LinkedQueue.java
+++ src/ds/queue/LinkedQueue.java
@@ -1,10 +1,20 @@
 package ds.queue;
 
 public class LinkedQueue <E> implements Queue <E> {
+	private class Node {
+		public E data;
+		public Node next;
+		public Node(E data, Node next) {
+			this.data = data;
+			this.next = next;
+		}
+	}
+	
+	public Node front = null;
+	private int size = 0;
 
 	@Override
 	public void enqueue(E item) {
-		// TODO Auto-generated method stub
 		
 	}
 
