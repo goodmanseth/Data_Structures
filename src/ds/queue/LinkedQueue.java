1a983bda375dae63138c6c46a9f0131151559b9f
Seth Goodman <sg277@duke.edu>
2017-04-05 15:30:55 -0400

    added file LinkedQueue.java

---
+++ src/ds/queue/LinkedQueue.java
@@ -0,0 +1,42 @@
+package ds.queue;
+
+public class LinkedQueue <E> implements Queue <E> {
+
+	@Override
+	public void enqueue(E item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public E dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public E peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
