417642f37dcef99c91c0e89f9efdffa6668372f6
Seth Goodman <sg277@duke.edu>
2017-04-05 15:17:02 -0400

    added documentation to the methods

--- src/ds/queue/Queue.java
+++ src/ds/queue/Queue.java
@@ -2,16 +2,48 @@ package ds.queue;
 
 public interface Queue <E> {
 	
+	/**
+	 * This method adds the item to the back of the queue.
+	 * 
+	 * @param  	item 	element to be added to queue.
+	 * @exception		java.util.IndexOutOfBoundsException if the queue is full (only for array implementation)
+	 */
 	void enqueue(E item);
 	
+	/**
+	 * This method removes and returns the first item in the queue.
+	 * 
+	 * @exception   	java.util.NoSuchElementException if the queue is empty.
+	 * @return 	item 	the first element in the queue. 
+	 */
 	E dequeue();
 	
+	/**
+	 * This method returns a boolean determining whether the stack is empty.
+	 * 
+	 * @return true,false
+	 */
 	boolean isEmpty();
 	
+	/**
+	 * This method returns an integer the size of the stack.
+	 * 
+	 * @return int  the size (length) of the stack
+	 */
 	int getSize();
 	
+	/**
+	 * This method returns the first item in the queue without removing it, allowing the 
+	 * user to see which element will be dequeued next.
+	 * 
+	 * @exception 		java.util.IndexOutOfBoundsException if the stack is empty.
+	 * @return  item	the last element added to the stack (the top element)
+	 */
 	E peek();
 	
+	/**
+	 * This method prints the contents of the queue to the console. 
+	 */
 	void display();
 	
 }
--- src/ds/stack/Stack.java
+++ src/ds/stack/Stack.java
@@ -3,35 +3,47 @@ package ds.stack;
 public interface Stack <E> {
 	
 	/**
-	 * Adds given integer to the top of the stack.
-	 * @exception java.util.IndexOutOfBoundsException if the stack is full (only for array implementation).
+	 * This method adds the given item to the top of the stack.
+	 * 
+	 * @param  item   element to be added to the top of the stack.
+	 * @exception     java.util.IndexOutOfBoundsException if the stack is full 
+	 * 				  (only for array implementation).
 	 */
 	void push(E item);
 	
 	/**
-	 * Removes and returns the top integer on the stack.
-	 * @exception java.util.NoSuchElementException if the stack is empty.
+	 * Removes and returns the top item on the stack.
+	 * 
+	 * @exception   	java.util.NoSuchElementException if the stack is empty.
+	 * @return  item	the last element added to the stack (the top element) 
 	 */
 	E pop();
 	
 	/**
-	 * Returns the top integer on the stack without removing it.
-	 * @exception java.util.NoSuchElementException if the stack is empty.
+	 * This method returns the top item on the stack without removing it, allowing the 
+	 * user to see which element will be popped off the stack next.
+	 * 
+	 * @exception 		java.util.IndexOutOfBoundsException if the stack is empty.
+	 * @return  item	the last element added to the stack (the top element)
 	 */
 	E peek();
 	
 	/**
-	 * Returns whether the stack is empty.
+	 * This method returns a boolean to determine whether the stack is empty.
+	 * 
+	 * @return true,false
 	 */
 	boolean isEmpty();
 		
 	/**
-	 * Returns an integer of the size of the stack.
+	 * This method returns an integer the size of the stack.
+	 * 
+	 * @return int  the size (length) of the stack
 	 */
 	int getSize();
 	
 	/**
-	 * Prints the contents of the stack to the console.
+	 * This method prints the contents of the stack to the console.
 	 */
 	void display();
 	
